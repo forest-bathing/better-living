6edd657f3478cfc6b874d65950123e700f719960
RuoLin-Li <amazingdudud@gmail.com>
2023-02-11 16:18:31 +0800

    33

--- js/badge.js
+++ js/badge.js
@@ -1,12 +1,10 @@
-
-let btn = document.querySelector("#show");
-let infoModal = document.querySelector("#infoModal");
-let close = document.querySelector("#close");
+let btn = document.querySelector(".show");
+let infoModal = document.querySelector(".infoModal");
+let close = document.querySelector(".close");
 btn.addEventListener("click", function () {
     infoModal.showModal();
 })
 close.addEventListener("click", function () {
     infoModal.close();
 })
-
 
