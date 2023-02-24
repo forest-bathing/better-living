e09baf95431406d7dab02b86bc1c9fe12487c61b
RuoLin-Li <amazingdudud@gmail.com>
2023-02-24 13:58:09 +0800

    38

--- js/ch.js
+++ js/ch.js
@@ -9,13 +9,3 @@ questions.forEach(function (question) { //選擇每個QA方格
         question.classList.toggle("show-text"); //就在<article class="question">新增/刪除"show-text"的Class
     });
 });
-
-questions.forEach(function (item) { //選擇每個QA方格
-
-    if (item !== question) { //只要不是被選擇的article
-
-        item.classList.remove("show-text"); //就將"show-text"的Class移除
-
-    };
-});
-
