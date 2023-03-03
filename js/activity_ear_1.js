b0b5c50036212e14692ece4d309049a6707490e4
RuoLin-Li <amazingdudud@gmail.com>
2023-03-03 17:37:01 +0800

    50

--- js/activity_ear_1.js
+++ js/activity_ear_1.js
@@ -4,11 +4,10 @@ const canvas = document.getElementById('draw');
 function drawCircle() {
     if (canvas.getContext) {
         var ctx = canvas.getContext('2d');
-        ctx.strokeStyle = '#1cea83';
-        ctx.fillStyle = '#94e5f7';
-        ctx.lineWidth = 10;
+        ctx.strokeStyle = '#ffffff';
+        ctx.fillStyle = '#ffffff'; 
         ctx.beginPath();
-        ctx.arc(200, 200, 40, 0, 2 * Math.PI);
+        ctx.arc(277, 667, 22, 0, 2 * Math.PI);
         ctx.closePath();
         ctx.fill();
         ctx.stroke();
@@ -17,24 +16,19 @@ function drawCircle() {
 
 function drawTriángulo() {
     if (canvas.getContext) {
-        let ctx = canvas.getContext('2d');
-        /*ctx.beginPath();
-        ctx.moveTo(50, 50);
-        ctx.lineTo(100, 100);
-        ctx.lineTo(50, 100);
+        var ctx = canvas.getContext('2d');
         ctx.beginPath();
-        ctx.fillStyle = '#1cea83';
-        ctx.fill();*/
-
-        ctx.beginPath();
+        ctx.strokeStyle = '#ffffff';
+        ctx.fillStyle = '#ffffff'; 
         ctx.moveTo(100, 50)
-        ctx.lineTo(200, 100)
+        ctx.lineTo(150, 100)
         ctx.lineTo(50, 100)
         //在用lineTo()把線連回原點的位置，圍成三角形
-        ctx.lineTo(50, 50)
+        ctx.lineTo(100, 50)
         /*而除了在連回原點外，也可以使用closePath()
         他會把最後一個點及起點座標相連，上下兩行擇一就行了！*/
         ctx.closePath()
+        ctx.fill();
         ctx.stroke()
     }
 }
@@ -42,8 +36,13 @@ function drawTriángulo() {
 function drawStrokeRect() {
     if (canvas.getContext) {
         let ctx = canvas.getContext('2d');
-        ctx.strokeStyle = '#1cea83';
-        ctx.strokeRect(200, 50, 50, 50);
+        ctx.beginPath();
+        ctx.strokeStyle = '#ffffff';
+        ctx.fillStyle = '#ffffff'; 
+        ctx.strokeRect(74, 514, 44, 44);
+        ctx.closePath()
+        ctx.fill();
+        ctx.stroke()
     }
 }
 
