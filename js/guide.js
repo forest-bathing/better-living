8865deef82e1de5f6d62b9095d56355fd3c2998c
RuoLin-Li <amazingdudud@gmail.com>
2023-03-06 20:07:45 +0800

    51

--- js/guide.js
+++ js/guide.js
@@ -1,68 +1,16 @@
-
-
-// 起始時間(計時器的啟動時間)。
-const startTime = new Date().getTime();
-
-// 目標時間(要倒數幾秒)。
-const targetSeconds = 120;
-
-// 初始化。
-init(targetSeconds);
-
-// timer.
-var timer = function (startTime) {
-  // 當前時間。
-  var currentTime = new Date().getTime();
-  
-  // 當前時間 - 起始時間 = 經過時間。(因為不需要毫秒，所以將結果除以1000。)
-  var diffSec = Math.round((currentTime - startTime) / 1000);
-  
-  // 目標時間 - 經過時間 = 剩餘時間。
-  var remainingTime = targetSeconds - diffSec;
-  
-  // update progess.  
-  update(remainingTime);   
-  
-  if (remainingTime == 0) {
-    // stop the timer.
-    clearInterval(timerId);
-    
-    // do anything you want to.
-    $(".msg").text("time up!");
-  } 
+var count = 1.02 ;
+function countDown(){
+	// 將秒數寫在指定元素中
+	document.getElementById("timeBox").innerHTML= count;
+	// 每次執行就減1
+	count -= 0.01;
+	// 當 count = 0 時跳轉頁面
+	if (count == 0){
+		location.href="https://kumo.tw/";
+	}
+	// 設定每秒執行1次
+	setTimeout("countDown()",1000);
 }
-
-// start the timer.
-var timerId = setInterval( function () { timer(startTime); }, 1000);
-
-// 初始化。此處借用update函式來初次設定進度條。
-function init(seconds) {
-  update(seconds);
-}
-
-// update progess with the timer.
-function update (seconds) {
-  barRenderer(seconds);
-  textRenderer(seconds);
-}
-
-// refresh the bar.
-function barRenderer (seconds) {
-  var percent = (seconds / targetSeconds) * 100;
-  //$(".bar").css("width",percent+"%");
-}
-
-// refresh the text of the bar.
-function textRenderer (seconds) {
-  var sec = seconds % 60;  
-  var min = Math.floor(seconds / 60); 
-
-  /* 兩種作法都可以 */
-  //min = min > 9 ? min : "0" + min;
-  //sec = sec > 9 ? sec : "0" + sec;  
-  min = min.toString().padStart(2, '0');
-  sec = sec.toString().padStart(2, '0');
-  
-  $('#text').text(min+':'+sec);		
-}
-
+// 執行 countDown
+countDown();
+setTimeout ("location.href='../activity_ear.html'",20000);
