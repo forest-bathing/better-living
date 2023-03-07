beac3cf494b518b097a4398a1d372b966613e198
RuoLin-Li <amazingdudud@gmail.com>
2023-03-07 14:01:15 +0800

    52

--- js/index.js
+++ js/index.js
@@ -1,5 +1,36 @@
-$(function () {
-    $('.carousel').carousel({
-        interval: 1000 // in milliseconds
-      })
-});
+let num = 0;
+let timer;
+ 
+function AddTimer(num) {
+    timer = setInterval(() => {
+        ChangePic(num);
+        num++;
+        if(num == 5) {
+            num = 0;
+        }
+    }, 5000);
+}
+ 
+function ChangePic(num) {
+    for(let i of document.querySelectorAll("li")) {
+        i.style.opacity = "0";
+    }
+    toRed(num);
+    document.querySelectorAll("li")[num].style.opacity = "1";
+}
+ 
+function toRed(num) {
+    for(let i of document.querySelectorAll("a")) {
+        i.style.backgroundColor = "grey";
+    }
+    document.querySelectorAll("a")[num].style.backgroundColor = "red";
+}
+AddTimer(num);
+
+    //  鼠标移入移开
+document.querySelector("div").addEventListener("mouseover", () => {
+    clearInterval(timer);
+})
+document.querySelector("div").addEventListener("mouseout", () => {
+    AddTimer(num);
+})
