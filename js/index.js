1a2e20d273dc95da9717aaf4cd82e4c576914728
RuoLin-Li <amazingdudud@gmail.com>
2023-03-08 10:06:37 +0800

    53

--- js/index.js
+++ js/index.js
@@ -1,36 +1,21 @@
-let num = 0;
-let timer;
- 
-function AddTimer(num) {
-    timer = setInterval(() => {
-        ChangePic(num);
-        num++;
-        if(num == 5) {
-            num = 0;
-        }
-    }, 5000);
-}
- 
-function ChangePic(num) {
-    for(let i of document.querySelectorAll("li")) {
-        i.style.opacity = "0";
-    }
-    toRed(num);
-    document.querySelectorAll("li")[num].style.opacity = "1";
-}
- 
-function toRed(num) {
-    for(let i of document.querySelectorAll("a")) {
-        i.style.backgroundColor = "grey";
-    }
-    document.querySelectorAll("a")[num].style.backgroundColor = "red";
-}
-AddTimer(num);
+var images = new Array('0.png', '1.png', '2.png');
+var index = 1;
 
-    //  鼠标移入移开
-document.querySelector("div").addEventListener("mouseover", () => {
-    clearInterval(timer);
-})
-document.querySelector("div").addEventListener("mouseout", () => {
-    AddTimer(num);
-})
+function rotateImage() {
+  $('#myImage').fadeOut('fast', function () {
+    $(this).attr('src', images[index]);
+
+    $(this).fadeIn('fast', function () {
+      if (index == images.length - 1) {
+        index = 0;
+      }
+      else {
+        index++;
+      }
+    });
+  });
+}
+
+$(document).ready(function () {
+  setInterval(rotateImage, 1000);
+});
