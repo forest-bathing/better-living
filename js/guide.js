615b46b11b1b589a4a89043d53ea9a5b92868e28
RuoLin-Li <amazingdudud@gmail.com>
2023-03-09 11:17:19 +0800

    63

--- js/guide.js
+++ js/guide.js
@@ -1,9 +1,9 @@
-var count = 1.02;
+var count = 1.020;
 function countDown() {
 	// 將秒數寫在指定元素中
 	document.getElementById("timeBox").innerHTML = count;
 	// 每次執行就減1
-	count -= 0.01;
+	count -= 0.010;
 	// 當 count = 0 時跳轉頁面
 	if (count == 0.00) {
 		location.href = "https://forest-bathing.github.io/better-living/activity_ear.html";
@@ -14,3 +14,7 @@ function countDown() {
 // 執行 countDown
 countDown();
 setTimeout("location.href='https://forest-bathing.github.io/better-living/activity_ear.html'", 51000);
+
+function roundToTwo(num) {
+    return +(Math.round(num + "e+2")  + "e-2");
+}
