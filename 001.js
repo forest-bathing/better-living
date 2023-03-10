493881c7c578bb700c97bc964aa4e67535f4c5a0
minmsl <4a81c031@stust.edu.tw>
2023-03-10 22:39:48 +0800

    73

--- 001.js
+++ 001.js
@@ -12,16 +12,16 @@
 
 // setInterval(nextSlide, 5000); *輪播
 
-// let countdown = 1.02; // 倒數計時的初始值
-// let countdownInterval = setInterval(function() {
-//   countdown = countdown - 0.01; // 每個時間間隔減少 0.01
-//   let countdownDisplay = countdown.toFixed(2); // 將倒數計時格式化為兩位小數的字符串
-//   document.getElementById("countdown").textContent = countdownDisplay; // 將格式化後的倒數計時顯示在頁面上
-//   if (countdown <= 0) {
-//     clearInterval(countdownInterval); // 如果倒數計時已經到達 0，就停止計時器
-//     document.getElementById("countdown").textContent = "Time's up!"; // 在頁面上顯示 "Time's up!"
-//   }
-// }, 10); // 每 10 毫秒執行一次 *倒數
+let countdown = 1.02; // 倒數計時的初始值
+let countdownInterval = setInterval(function() {
+  countdown = countdown - 0.01; // 每個時間間隔減少 0.01
+  let countdownDisplay = countdown.toFixed(2); // 將倒數計時格式化為兩位小數的字符串
+  document.getElementById("countdown").textContent = countdownDisplay; // 將格式化後的倒數計時顯示在頁面上
+  if (countdown <= 0) {
+    clearInterval(countdownInterval); // 如果倒數計時已經到達 0，就停止計時器
+    document.getElementById("countdown").textContent = "0.00"; // 在頁面上顯示 "Time's up!"
+  }
+}, 50); // 每 10 毫秒執行一次 *倒數
 
 // let isDragging = false;
 // let box = document.getElementById("box");
