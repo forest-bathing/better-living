a0da214c4d57c856219521e76e1afeb811b872fd
RuoLin-Li <amazingdudud@gmail.com>
2023-03-12 14:01:03 +0800

    105

--- js/guide_heart.js
+++ js/guide_heart.js
@@ -6,7 +6,7 @@ let countdownInterval = setInterval(function () {
   countdown = countdown - 0.01; // 每個時間間隔減少 0.01
   let countdownDisplay = countdown.toFixed(2); // 將倒數計時格式化為兩位小數的字符串
   document.getElementById("countdown").textContent = countdownDisplay; // 將格式化後的倒數計時顯示在頁面上
-  if (countdown <= -0.00) {
+  if (countdown == -0.00) {
     location.href = "https://forest-bathing.github.io/better-living/activity_mood.html";
   }
 }, 50); 
